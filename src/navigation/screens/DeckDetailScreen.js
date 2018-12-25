4d52d21e96ecb226ae7e9f820706ec2d19973a8d
Unknown <ademouse@gmail.com>
2018-12-25 21:44:28 +0100

    DeckDetailScreen basic buttons and space distribution

--- src/navigation/screens/DeckDetailScreen.js
+++ src/navigation/screens/DeckDetailScreen.js
@@ -34,12 +34,21 @@ export default class DeckDetailScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <View style={{alignItems: 'center', paddingTop: 10}}>
-          <Text style={styles.textTitle}>Your decks list</Text>
-        </View> */}
-        <View style={styles.deckInfo}>
-          <Text style={styles.textTitle}>{this.getNavigationParams().deck[1].title}</Text>
-          <Text style={styles.textInfo}>{this.getNavigationParams().deck[1].questions.length} cards</Text>
+        <View style={styles.deck}>
+          <View style={styles.deckInfo}>
+            <Text style={styles.textTitle}>{this.getNavigationParams().deck[1].title}</Text>
+            <Text style={styles.textInfo}>{this.getNavigationParams().deck[1].questions.length} cards</Text>
+          </View>
+          <View style={styles.actionButtons}>
+            <Button
+              onPress={()=>console.log("go Add card")}
+              title="Add card"
+            />
+            <Button
+              onPress={()=>console.log("go Start Quiz")}
+              title="Start Quiz"
+            />
+          </View>
         </View>
       </View>
     )
@@ -60,10 +69,11 @@ const styles = StyleSheet.create({
   textInfo: {
     color: 'grey',
   },
-  deckInfo: {
+  deck: {
     flex: 1,
     backgroundColor: '#DDEBF8',
     alignItems: 'center',
+    justifyContent: 'space-between',
     margin: 10,
     marginLeft: 15,
     marginRight: 15,
@@ -71,5 +81,18 @@ const styles = StyleSheet.create({
   },
   touchableOpacity: {
     backgroundColor: '#ABCDEF',
+  },
+  deckInfo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionButtons: {
+    flex: 1,
+    // alignItems: 'stretch', // NOTE why
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    padding: 5,
+    backgroundColor: '#FBEBF3'
   },
 });
