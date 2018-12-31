2f9b6bc76f5a682a904d191c675adb42a8861c2e
Unknown <ademouse@gmail.com>
2018-12-31 21:22:39 +0100

    QuizScreen basic change of cards

--- src/navigation/screens/QuizScreen.js
+++ src/navigation/screens/QuizScreen.js
@@ -5,43 +5,64 @@ import {
   View,
   Button,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 
-export default QuizScreen = (props) => {
-  const getNavParamDeck = props.navigation.getParam('deck');
-  return (
-    <View style={styles.container}>
-      <View style={styles.deck}>
-        <View style={{flex: 1, alignSelf: 'stretch'}}>
-          <Text style={styles.textCardNumber}>{"1/"+getNavParamDeck.questions.length}</Text>
-          <Text style={styles.textTitle}>{getNavParamDeck.title}</Text>
-        </View>
-        <View style={{flex: 3}}>
-          <Text style={styles.textQuestion}>{getNavParamDeck.questions[0].question}</Text>
-          <Button
-            onPress={() => console.log("go Answer")}
-            title="Answer"
-          />
-        </View>
-        <View style={styles.actionButtons}>
-          <TouchableOpacity 
-            style={styles.incorrectButton}
-            activeOpacity={0.5}
-            onPress = {() => console.log("go Incorrect")}
-            >
-            <Text style={styles.textResponseButton}>{"Incorrect"}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.correctButton}
-            activeOpacity={0.5}
-            onPress = {() => console.log("go Correct")}
-            >
-            <Text style={styles.textResponseButton}>{"Correct"}</Text>
-          </TouchableOpacity>
+// Displays the percentage correct once the quiz is complete
+// DONE the number of cards left in the quiz
+export default class QuizScreen extends React.Component {
+  // getNavParamDeck = this.props.navigation.getParam('deck');
+  
+  state = {
+    getNavParamDeck: this.props.navigation.getParam('deck'),
+    cardCounter: 0,
+  }
+  
+  onCorrect = () => {
+    if(this.state.cardCounter <= this.state.getNavParamDeck.questions.length - 2){
+      this.setState((prevState) => {
+        return ({ cardCounter: prevState.cardCounter + 1 });
+      });
+    }else{
+      Alert.alert('This deck do not have more cards');
+    }
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.deck}>
+          <View style={{flex: 1, alignSelf: 'stretch'}}>
+            <Text style={styles.textCardNumber}>{(this.state.cardCounter+1)+"/"+this.state.getNavParamDeck.questions.length}</Text>
+            <Text style={styles.textTitle}>{this.state.getNavParamDeck.title}</Text>
+          </View>
+          <View style={{flex: 3}}>
+            <Text style={styles.textQuestion}>{this.state.getNavParamDeck.questions[this.state.cardCounter].question}</Text>
+            <Button
+              onPress={() => console.log("go Answer")}
+              title="Answer"
+            />
+          </View>
+          <View style={styles.actionButtons}>
+            <TouchableOpacity 
+              style={styles.incorrectButton}
+              activeOpacity={0.5}
+              onPress = {() => console.log("go Incorrect")}
+              >
+              <Text style={styles.textResponseButton}>{"Incorrect"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={styles.correctButton}
+              activeOpacity={0.5}
+              onPress = {this.onCorrect}
+              >
+              <Text style={styles.textResponseButton}>{"Correct"}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 QuizScreen.navigationOptions = {
