9375fb8cb1542e76c5b93e7ec95d1048ebab848f
Unknown <ademouse@gmail.com>
2018-12-31 21:59:56 +0100

    Extract first Card Component from QuizScreen

---
+++ src/Components/Card.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+export default Card = (props) => { 
+  // console.log(props);
+  return (
+  <View style={styles.card}>
+    <View style={{flex: 1, alignSelf: 'stretch'}}>
+      <Text style={styles.textCardNumber}>{(props.cardCounter+1)+"/"+props.navParamDeck.questions.length}</Text>
+      <Text style={styles.textTitle}>{props.navParamDeck.title}</Text>
+    </View>
+    <View style={{flex: 3}}>
+      <Text style={styles.textQuestion}>{props.navParamDeck.questions[props.cardCounter].question}</Text>
+      <Button
+        onPress={() => console.log("go Answer")}
+        title="Answer"
+      />
+    </View>
+    <View style={styles.actionButtons}>
+      <TouchableOpacity 
+        style={styles.incorrectButton}
+        activeOpacity={0.5}
+        onPress = {() => console.log("go Incorrect")}
+        >
+        <Text style={styles.textResponseButton}>{"Incorrect"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        style={styles.correctButton}
+        activeOpacity={0.5}
+        onPress = {props.onCorrect}
+        >
+        <Text style={styles.textResponseButton}>{"Correct"}</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+)};
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: '#f1f1ba',
+    alignItems: 'center',
+    margin: 10,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  textTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#719e71',
+    alignSelf: 'center',
+  },
+  textQuestion: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  textCardNumber: {
+    fontSize: 17,
+    color: 'grey',
+    alignSelf: 'flex-start',
+    margin: 10,
+    marginLeft: 15,
+  },
+  actionButtons: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    padding: 5,
+  },
+  correctButton: {
+    backgroundColor: '#9ad89a',
+    padding: 15,
+    borderRadius: 10,
+    margin: 5,
+  },
+  incorrectButton: {
+    backgroundColor: '#d89a9a',
+    padding: 15,
+    borderRadius: 10,
+    margin: 5,
+  },
+  textResponseButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
--- src/navigation/screens/QuizScreen.js
+++ src/navigation/screens/QuizScreen.js
@@ -7,19 +7,19 @@ import {
   TouchableOpacity,
   Alert,
 } from 'react-native';
+import { Card } from '../../Components';
 
 // Displays the percentage correct once the quiz is complete
 // DONE the number of cards left in the quiz
 export default class QuizScreen extends React.Component {
-  // getNavParamDeck = this.props.navigation.getParam('deck');
-  
   state = {
-    getNavParamDeck: this.props.navigation.getParam('deck'),
     cardCounter: 0,
   }
   
+  navParamDeck = this.props.navigation.getParam('deck');
+  
   onCorrect = () => {
-    if(this.state.cardCounter <= this.state.getNavParamDeck.questions.length - 2){
+    if(this.state.cardCounter <= this.navParamDeck.questions.length - 2){
       this.setState((prevState) => {
         return ({ cardCounter: prevState.cardCounter + 1 });
       });
@@ -31,35 +31,11 @@ export default class QuizScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.deck}>
-          <View style={{flex: 1, alignSelf: 'stretch'}}>
-            <Text style={styles.textCardNumber}>{(this.state.cardCounter+1)+"/"+this.state.getNavParamDeck.questions.length}</Text>
-            <Text style={styles.textTitle}>{this.state.getNavParamDeck.title}</Text>
-          </View>
-          <View style={{flex: 3}}>
-            <Text style={styles.textQuestion}>{this.state.getNavParamDeck.questions[this.state.cardCounter].question}</Text>
-            <Button
-              onPress={() => console.log("go Answer")}
-              title="Answer"
-            />
-          </View>
-          <View style={styles.actionButtons}>
-            <TouchableOpacity 
-              style={styles.incorrectButton}
-              activeOpacity={0.5}
-              onPress = {() => console.log("go Incorrect")}
-              >
-              <Text style={styles.textResponseButton}>{"Incorrect"}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity 
-              style={styles.correctButton}
-              activeOpacity={0.5}
-              onPress = {this.onCorrect}
-              >
-              <Text style={styles.textResponseButton}>{"Correct"}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <Card
+          cardCounter={this.state.cardCounter}
+          navParamDeck={this.navParamDeck}
+          onCorrect={this.onCorrect}
+        />
       </View>
     );
   }
